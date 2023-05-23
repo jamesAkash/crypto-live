3b86392d43e123799d6fddb83c3aa9916c6db750
jamesAkash <akash.james@outlook.com>
2023-05-24 01:39:25 +0530

    added page structures and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./Components";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import {
+  Home,
+  News,
+  Exchanges,
+  CryptoDetails,
+  Cryptocurrencies,
+} from "./pages";
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            LiveCrypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
